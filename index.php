1fdf2b3980f30623ffbde051893f2ba4bad4aedd
Tixis <Contact@tixis.org>
2014-07-02 18:11:50 +0200

    Add index.php and requete.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<html>
+
+
+	<head>
+
+		<title>FindIT</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+	    <link rel="icon" type="image/png" href="icon.png" />
+
+	</head>
+
+	<body>
+
+	<?php
+
+	 //Initialisation des variables pour la géolocalisation
+
+	   $geoPlugin_array = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' . $_SERVER['REMOTE_ADDR']));
+	   $user_lat = $geoPlugin_array['geoplugin_latitude'];
+	   $user_lng = $geoPlugin_array['geoplugin_longitude'];
+
+	   $user_lat = round($geoPlugin_array['geoplugin_latitude']);
+	   $user_lng = round($geoPlugin_array['geoplugin_longitude']);
+
+	 //Mise en place de l'API Deezer
+
+	   $app_id = "SECRET_DEFENSE"; //A changer
+	   $secret = "DESOLE_VOUS_N'AUREZ_PAS_NOS_CODES :p"; // A changer
+				
+	   $redirect_uri = "http://tixis-lab.com/FindIT/";
+	   $url_oauth = "https://connect.deezer.com/oauth/auth.php?app_id=" . $app_id . "&redirect_uri=".$redirect_uri."&perms=basic_access,email,manage_library,offline_access ";
+	   $url_access_token = "http://connect.deezer.com/oauth/access_token.php?app_id=". $app_id ."&secret=". $secret ."&code=". $_GET['code'] ."";
+
+	  //Mise en place du bouton et de l'access_token
+
+	   echo '<center><a href="'. $url_oauth .'"><img src="http://upload.wikimedia.org/wikipedia/fr/0/00/Deezer_logo.png"></img></a></center>';
+
+	   $access_token = file_get_contents($url_access_token);
+	   $access_token = str_replace("access_token=", "", $access_token);
+	   $access_token = str_replace("&expires=0", "", $access_token);
+
+	  //Si l'access_token est = à wrong code alors on ne fais rien sinon on inclus la page requete.php
+
+	   if ($access_token=="wrong code"){
+
+	            
+		            
+	   }else{
+
+	     include("requete.php");
+
+	   }
+
+	?>
+
+	<center>
+
+		<u><h3>Social : </h3></u>
+
+		<br/>
+
+		<a href="https://twitter.com/share" class="twitter-share-button" data-text="Je viens de tester un super système pour trouver des places de concert près de chez soi" data-lang="fr" data-size="large">Tweeter</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FTixis%2F140148279427566%3Fref%3Dbr_tf&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=215414585257094" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
+	
+	</center>
+
+	</body>
+
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</html>
+
+<!--   ©Tixis ~ 2014-->
---
+++ requete.php
@@ -0,0 +1,119 @@
+﻿<meta charset="utf-8">
+
+<center>
+
+<?php
+
+	 //Initialisation des requêtes pour les artistes de Deezer
+
+	 $url_requete_artistes = "http://api.deezer.com/user/me/artists?access_token=" . $access_token;
+	 $requete_artistes = file_get_contents($url_requete_artistes);
+	 $requete_json_artistes = json_decode($requete_artistes);
+
+	 //Mise en place de la boucle pour récupérer tout les artistes et les tester	
+
+	 $count_artistes = $requete_json_artistes->total;
+	 $count_artistes = $count_artistes-2;
+	 $i = -1;
+
+	 //Tant que $i est inférieur ou égal à $count_artistes alors on fetch les valeurs des artistes 
+
+	  while($i<=$count_artistes){
+
+	 	$i = $i+1;
+
+	 	$artistes_nom = $requete_json_artistes->data[$i]->name;
+
+	 	$artistes_image = $requete_json_artistes->data[$i]->picture;
+
+	 	$array_artistes = array(
+
+	 		"nom" => $artistes_nom,
+
+	 		"image" => $artistes_image,
+
+	 	);
+
+	 	//Mise en place de l'API Songkick
+
+		$url_requete_songkick = "http://api.songkick.com/api/3.0/events.json?apikey=API_SONGKICK&artist_name=".$artistes_nom."";
+		$sortie_songkick = file_get_contents($url_requete_songkick);
+		$sortie_songkick_json = json_decode($sortie_songkick);
+		$count_songkick = $sortie_songkick_json->resultsPage->totalEntries;
+   		$count_songkick = $count_songkick-2;
+   		$i_songkick = 0;
+
+   		while ($i_songkick <= $count_songkick){
+
+   			$i_songkick=$i_songkick+1;
+
+			$concerts_titre =  $sortie_songkick_json->resultsPage->results->event[$i_songkick]->displayName;
+		   	$concerts_lieu =  $sortie_songkick_json->resultsPage->results->event[$i_songkick]->location->city;
+		   	$concerts_date =  $sortie_songkick_json->resultsPage->results->event[$i_songkick]->start->date;
+		   	$concerts_heure =  $sortie_songkick_json->resultsPage->results->event[$i_songkick]->start->time;
+		   	$concerts_lat =  $sortie_songkick_json->resultsPage->results->event[$i_songkick]->location->lat;
+		   	$concerts_long =  $sortie_songkick_json->resultsPage->results->event[$i_songkick]->location->lng;
+		   	$concerts_uri =  $sortie_songkick_json->resultsPage->results->event[$i_songkick]->uri;
+
+		   	$array_concerts = array(
+
+		   	 		"titre" => $concerts_titre,
+
+		   	 		"lieu" => $concerts_lieu,
+
+		   	 		"date" => $concerts_date,
+
+		   	 		"heure" => $concerts_heure,
+
+		   	 		"latitude" => $concerts_lat,
+
+		   	 		"longitude" => $concerts_long,
+
+		   	 		"lien" => $concerts_uri,
+
+		   	);
+
+		   	$concerts_lat = round($concerts_lat);
+			$concerts_long = round($concerts_long);
+
+	 		if($concerts_titre!=NULL){
+
+	 			echo '<u><h1>'.$artistes_nom.' : </h1></u>';
+
+			    echo '<img src='.$artistes_image.'></img>';
+
+			    echo '<br/>';
+			 	
+			 	echo '<br/>';
+
+	 			echo '<p>Le concert ('.$concerts_titre.') sera le '.$concerts_date.' à '.$concerts_heure.' et se trouvera à '.$concerts_lieu.'</p>';
+	 			
+	 			echo '<br/>';
+
+	 			echo '<img style="max-width: 100%;height: auto;-webkit-border-radius: 15px;-moz-border-radius: 15px;border-radius: 15px;" border="0" src=http://maps.googleapis.com/maps/api/staticmap?center='.$concerts_lat.','.$concerts_long.'&zoom=13&size=600x300&maptype=roadmap&markers=color:blue|label:S|'.$concerts_lat.','.$concerts_long.'></img>';		
+
+	 			echo '<br/>';
+
+	 			echo '<br/>';
+
+	 			echo '<a href="'.$concerts_uri.'"><p>Réserver une place</p></a>';
+
+				if($concerts_lat==$user_lat&&$concerts_long==$user_lng){
+
+					echo '<u style="color : red;"><h3 style="color : red;"><img src="http://findicons.com/files/icons/1034/elementary/16/emblem_danger.png"></img> Ce concert se trouve près de chez vous <img src="http://findicons.com/files/icons/1034/elementary/16/emblem_danger.png"></img></h3></u>';
+
+				}
+
+			    echo '<hr>';
+
+			 }
+
+	 }
+
+}
+
+?>
+
+</center>
+
+<!--   ©Tixis ~ 2014-->
